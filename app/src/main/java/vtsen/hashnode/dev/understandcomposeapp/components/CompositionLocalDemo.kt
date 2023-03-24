1e9115857c160b19f38b2ab274c6cf09a2427477
vinchamp77 <vinchamp77@gmail.com>
2023-03-24 15:39:21 +1100

    Update composition local

--- app/src/main/java/vtsen/hashnode/dev/understandcomposeapp/components/CompositionLocalDemo.kt
+++ app/src/main/java/vtsen/hashnode/dev/understandcomposeapp/components/CompositionLocalDemo.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.*
 
 private val LocalInt = compositionLocalOf { 0 }
+private val tag = "CompLocal"
 
 @Composable
 fun CompositionLocalDemo() {
@@ -14,16 +15,20 @@ fun CompositionLocalDemo() {
 
     MyButton(onClick = { ++counter }, text = "CompositionLocal Demo")
 
+    Log.d(tag, "************** Using CompositionLocal **************")
     CompositionLocalProvider(
         LocalInt provides counter,
     ) {
         Parent()
     }
+    Log.d(tag, "************** Pass by Value **************")
+
+    Parent(counter)
 }
 
 @Composable
 private fun Parent() {
-    Log.d("LocalInt", "Enter Parent - LocalInt: ${LocalInt.current} ")
+    Log.d(tag, "Enter Parent - LocalInt: ${LocalInt.current} ")
 
     CompositionLocalProvider(
         LocalInt provides LocalInt.current + 1,
@@ -31,17 +36,37 @@ private fun Parent() {
         Child()
     }
 
-    Log.d("LocalInt", "Exit Parent - LocalInt: ${LocalInt.current}")
+    Log.d(tag, "Exit Parent - LocalInt: ${LocalInt.current}")
 }
 
 @Composable
 private fun Child() {
-    Log.d("LocalInt", "Child - LocalInt: ${LocalInt.current} ")
+    Log.d(tag, "Enter Child - LocalInt: ${LocalInt.current} ")
 
     GrandChild()
+
+    Log.d(tag, "Exit Child - LocalInt: ${LocalInt.current} ")
 }
 
 @Composable
 private fun GrandChild() {
-    Log.d("LocalInt", "GarndChild")
+    Log.d(tag, "Enter GrandChild")
+}
+
+@Composable
+private fun Parent(value: Int) {
+    Log.d(tag, "Enter Parent - value: $value")
+
+    Child(value + 1)
+
+    Log.d(tag, "Exit Parent - value: $value")
+}
+
+@Composable
+private fun Child(value: Int) {
+    Log.d(tag, "Enter Child - value: $value")
+
+    GrandChild()
+
+    Log.d(tag, "Exit Child - value: $value")
 }
--- app/src/main/java/vtsen/hashnode/dev/understandcomposeapp/components/StaticCompositionLocalDemo.kt
+++ app/src/main/java/vtsen/hashnode/dev/understandcomposeapp/components/StaticCompositionLocalDemo.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.*
 
 private val LocalInt = staticCompositionLocalOf { 0 }
+private val tag = "StaticCompLocal"
 
 @Composable
 fun StaticCompositionLocalDemo() {
@@ -12,18 +13,22 @@ fun StaticCompositionLocalDemo() {
         mutableStateOf(0)
     }
 
-    MyButton(onClick = { ++counter }, text = "CompositionLocal Demo")
+    MyButton(onClick = { ++counter }, text = "StaticCompositionLocal Demo")
 
+    Log.d(tag, "************** Using StaticCompositionLocal **************")
     CompositionLocalProvider(
         LocalInt provides counter,
     ) {
         Parent()
     }
+    Log.d(tag, "************** Pass by Value **************")
+
+    Parent(counter)
 }
 
 @Composable
 private fun Parent() {
-    Log.d("LocalInt", "Enter Parent - LocalInt: ${LocalInt.current} ")
+    Log.d(tag, "Enter Parent - LocalInt: ${LocalInt.current} ")
 
     CompositionLocalProvider(
         LocalInt provides LocalInt.current + 1,
@@ -31,17 +36,37 @@ private fun Parent() {
         Child()
     }
 
-    Log.d("LocalInt", "Exit Parent - LocalInt: ${LocalInt.current}")
+    Log.d(tag, "Exit Parent - LocalInt: ${LocalInt.current}")
 }
 
 @Composable
 private fun Child() {
-    Log.d("LocalInt", "Child - LocalInt: ${LocalInt.current} ")
+    Log.d(tag, "Enter Child - LocalInt: ${LocalInt.current} ")
 
     GrandChild()
+
+    Log.d(tag, "Exit Child - LocalInt: ${LocalInt.current} ")
 }
 
 @Composable
 private fun GrandChild() {
-    Log.d("LocalInt", "GarndChild")
+    Log.d(tag, "Enter GrandChild")
+}
+
+@Composable
+private fun Parent(value: Int) {
+    Log.d(tag, "Enter Parent - value: $value")
+
+    Child(value + 1)
+
+    Log.d(tag, "Exit Parent - value: $value")
+}
+
+@Composable
+private fun Child(value: Int) {
+    Log.d(tag, "Enter Child - value: $value")
+
+    GrandChild()
+
+    Log.d(tag, "Exit Child - value: $value")
 }
